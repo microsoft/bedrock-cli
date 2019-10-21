c795fac071424e118ef29515ce8ec56204cb821f
Nate <NathanielRose@users.noreply.github.com>
2019-10-21 09:22:33 -0700

    removal of infra create (#86)

--- src/commands/infra/index.ts
+++ src/commands/infra/index.ts
@@ -1,10 +1,9 @@
 import { Command } from "../command";
-import { createCommandDecorator } from "./create";
 import { scaffoldCommandDecorator } from "./scaffold";
 import { validateCommandDecorator } from "./validate";
 
 export const infraCommand = Command(
   "infra",
   "Manage and modify your Bedrock infrastructure.",
-  [createCommandDecorator, scaffoldCommandDecorator, validateCommandDecorator]
+  [scaffoldCommandDecorator, validateCommandDecorator]
 );
