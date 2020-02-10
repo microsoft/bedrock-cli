745598b587877b28a3c4c44ef68a0ed6492eaf6f
Dennis Seah <dennis.seah@gmail.com>
2020-02-10 10:47:33 -0800

    [REFACTOR] spk variable-group create command (#276)

--- src/commands/variable-group/create.ts
+++ src/commands/variable-group/create.ts
@@ -1,8 +1,8 @@
-import { VariableGroup } from "azure-devops-node-api/interfaces/ReleaseInterfaces";
 import commander from "commander";
 import fs from "fs";
 import path from "path";
 import { readYaml } from "../../config";
+import { build as buildCmd, exit as exitCmd } from "../../lib/commandBuilder";
 import { IAzureDevOpsOpts } from "../../lib/git";
 import {
   addVariableGroup,
@@ -10,87 +10,69 @@ import {
 } from "../../lib/pipelines/variableGroup";
 import { logger } from "../../logger";
 import { IVariableGroupData } from "../../types";
+import decorator from "./create.decorator.json";
 
 /**
  * Adds the create command to the variable-group command object
  *
  * @param command Commander command object to decorate
  */
-export const createCommandDecorator = (command: commander.Command): void => {
-  command
-    .command("create")
-    .alias("c")
-    .description("Add a new variable group in Azure DevOps project.")
-    .option(
-      "-f, --file <variable-group-manifest-file-path>",
-      "Path to the yaml file that contains variable group manifest."
-    )
-    .option(
-      "-o, --org-name <organization-name>",
-      "Azure DevOps organization name; falls back to azure_devops.org in spk config"
-    )
-    .option(
-      "-p, --project <project>",
-      "Azure DevOps project name; falls back to azure_devops.project in spk config"
-    )
-    .option(
-      "-t, --personal-access-token <personal-access-token>",
-      "Personal access token associated with the Azure DevOps org; falls back to azure_devops.access_token in spk config"
-    )
-    .action(async opts => {
-      try {
-        if (!opts.file) {
-          logger.error(
-            "You need to specify a file with variable group manifest"
-          );
-          return;
-        }
-
-        const { file, orgName, project, personalAccessToken } = opts;
-
-        logger.debug(
-          `opts: ${file}, ${orgName}, ${project}, ${personalAccessToken}`
+export const commandDecorator = (command: commander.Command): void => {
+  buildCmd(command, decorator).action(async opts => {
+    try {
+      if (!opts.file) {
+        throw new Error(
+          "You need to specify a file with variable group manifest"
         );
-
-        // type check
-        if (typeof orgName !== "undefined" && typeof orgName !== "string") {
-          throw Error(
-            `--org-name must be of type 'string', ${typeof orgName} specified.`
-          );
-        }
-
-        if (typeof project !== "undefined" && typeof project !== "string") {
-          throw Error(
-            `--project must be of type 'string', ${typeof project} specified.`
-          );
-        }
-
-        if (
-          typeof personalAccessToken !== "undefined" &&
-          typeof personalAccessToken !== "string"
-        ) {
-          throw Error(
-            `--personal-access-token must be of type 'string', ${typeof personalAccessToken} specified.`
-          );
-        }
-
-        const accessOpts: IAzureDevOpsOpts = {
-          orgName,
-          personalAccessToken,
-          project
-        };
-        logger.debug(`access options: ${JSON.stringify(accessOpts)}`);
-
-        await create(opts.file, accessOpts);
-
-        logger.info(
-          "Successfully added a variable group in Azure DevOps project!"
-        );
-      } catch (err) {
-        logger.error(`Error occurred while creating variable group`);
-        logger.error(err);
       }
-    });
+
+      const { file, orgName, project, personalAccessToken } = opts;
+
+      logger.debug(
+        `opts: ${file}, ${orgName}, ${project}, ${personalAccessToken}`
+      );
+
+      // type check
+      if (typeof orgName !== "undefined" && typeof orgName !== "string") {
+        throw Error(
+          `--org-name must be of type 'string', ${typeof orgName} specified.`
+        );
+      }
+
+      if (typeof project !== "undefined" && typeof project !== "string") {
+        throw Error(
+          `--project must be of type 'string', ${typeof project} specified.`
+        );
+      }
+
+      if (
+        typeof personalAccessToken !== "undefined" &&
+        typeof personalAccessToken !== "string"
+      ) {
+        throw Error(
+          `--personal-access-token must be of type 'string', ${typeof personalAccessToken} specified.`
+        );
+      }
+
+      const accessOpts: IAzureDevOpsOpts = {
+        orgName,
+        personalAccessToken,
+        project
+      };
+      logger.debug(`access options: ${JSON.stringify(accessOpts)}`);
+
+      await create(opts.file, accessOpts);
+
+      logger.info(
+        "Successfully added a variable group in Azure DevOps project!"
+      );
+      await exitCmd(logger, process.exit, 0);
+    } catch (err) {
+      logger.error(`Error occurred while creating variable group`);
+      logger.error(err);
+      await exitCmd(logger, process.exit, 1);
+    }
+  });
 };
 
 /**
@@ -106,25 +88,19 @@ export const create = async (
   logger.info(
     `Creating Variable Group from group definition '${path.resolve(filepath)}'`
   );
-  try {
-    fs.statSync(filepath);
-    const data = readYaml<IVariableGroupData>(filepath);
-    logger.debug(`Variable Group Yaml data: ${JSON.stringify(data)}`);
 
-    // validate variable group type
+  fs.statSync(filepath);
+  const data = readYaml<IVariableGroupData>(filepath);
+  logger.debug(`Variable Group Yaml data: ${JSON.stringify(data)}`);
 
-    let variableGroup: VariableGroup;
-
-    if (data.type === "AzureKeyVault") {
-      variableGroup = await addVariableGroupWithKeyVaultMap(data, accessOpts);
-    } else if (data.type === "Vsts") {
-      variableGroup = await addVariableGroup(data, accessOpts);
-    } else {
-      throw new Error(
-        `Variable Group type "${data.type}" is not supported. Only "Vsts" and "AzureKeyVault" are valid types and case sensitive.`
-      );
-    }
-  } catch (err) {
-    throw err;
+  // validate variable group type
+  if (data.type === "AzureKeyVault") {
+    await addVariableGroupWithKeyVaultMap(data, accessOpts);
+  } else if (data.type === "Vsts") {
+    await addVariableGroup(data, accessOpts);
+  } else {
+    throw new Error(
+      `Variable Group type "${data.type}" is not supported. Only "Vsts" and "AzureKeyVault" are valid types and case sensitive.`
+    );
   }
 };
--- src/commands/variable-group/index.ts
+++ src/commands/variable-group/index.ts
@@ -1,5 +1,5 @@
 import { Command } from "../command";
-import { createCommandDecorator } from "./create";
+import { commandDecorator as createCommandDecorator } from "./create";
 export const variableGroupCommand = Command(
   "variable-group",
   "Creates Variable Group in Azure DevOps project.",
