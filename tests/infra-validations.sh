88f04f8e63af92981e347269a84d191dc3dc8136
nrose <rose.nathanielm@gmail.com>
2020-04-28 15:20:45 -0700

    More checks

--- tests/infra-validations.sh
+++ tests/infra-validations.sh
@@ -72,7 +72,7 @@ mkdir module
 
 # Configure Validation Terraform files
 module=$'provider "azurerm" {\n   features {}\n}\nresource "azurerm_resource_group" "resource_group"{\n  name= var.rg_name\n  location = var.rg_location\n}'
-tfTemplate=$'provider "azurerm" {\n   features {}\n}\nresource "azurerm_resource_group" "example"{\n  name= var.rg_name\n  location = var.rg_location\n}\nmodule "resource_group" {\n  source = "../module"\n  rg_name= "local_test"\n  rg_location = "eastus"\n}'
+tfTemplate=$'provider "azurerm" {\n   features {}\n}\nresource "azurerm_resource_group" "example"{\n  name= var.rg_name\n  location = var.rg_location\n}\nmodule "resource_group" {\n  source= "../module"\n  rg_name= "local_test"\n  rg_location = "eastus"\n}'
 tfVars=$'variable "rg_name" {\n  type = "string"\n}\n\nvariable "rg_location" {\n  type = "string"\n}\n'
 backendTfVars=$'storage_account_name="<storage account name>"'
 touch template/main.tf template/variables.tf template/backend.tfvars
